f7b39a37e87e5816582c9fc143b0aefdef76c028
{authemail@qq.com} <authemail@qq.com>
2023-04-15 12:35:45 +0800

    chore: umijs4

---
+++ packages/ui/dist/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var jsxRuntime = require('react/jsx-runtime');
+
+const Button = () => {
+    return jsxRuntime.jsx("button", { children: "Boop" });
+};
+
+exports.Button = Button;
--- packages/ui/rollup.config.js
+++ packages/ui/rollup.config.js
@@ -2,9 +2,15 @@ import typescript from "@rollup/plugin-typescript";
 
 export default {
   input: "index.tsx",
-  output: {
-    file: "dist/index.js",
-    format: "cjs",
-  },
+  output: [
+    {
+      file: "dist/index.js",
+      format: "cjs",
+    },
+    {
+      file: "dist/index.esm.js",
+      format: "esm",
+    }
+  ],
   plugins: [typescript()],
 };
